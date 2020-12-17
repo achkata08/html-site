610c95ab65d5a877b0741947abcad13994ec21da
Angel Angelov <aangelov8383@gmail.com>
2020-12-17 17:09:26 +0200

    Include header and footer with PHP #3

---
+++ components/header.php
@@ -0,0 +1,21 @@
+<header class="header">
+        <nav class="navbar">
+            <ul class="navbar-ul base">
+                <li><a class="link" href="/">Home</a></li>
+                <li><a class="link" href="/about.php">About</a></li>
+            </ul>
+            <ul class="navbar-ul hamburger">
+                <li class="navbar-ul">
+                    <a href="javascript:void(0);" class="link"  onclick="hamburgeFunc()">
+                        <i class="fa fa-bars"></i>
+                    </a>
+                </li>
+                <div class="hamburger-menu" id="hamb">
+                    <ul>
+                        <li><a class="link" href="/">Home</a></li>
+                        <li><a class="link" href="/about.php">About</a></li>
+                    </ul>
+                </div>
+            </ul>
+        </nav>
+    </header>
