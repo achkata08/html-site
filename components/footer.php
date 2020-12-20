1d9192433c6ae9b371c814750a365de44509590e
Angel Angelov <aangelov8383@gmail.com>
2020-12-20 17:25:34 +0200

    I make a change in the footer.

--- components/footer.php
+++ components/footer.php
@@ -4,5 +4,7 @@
         <i class="fab fa-linkedin"></i>
         <i class="fab fa-instagram"></i>
         <i class="fas fa-envelope"></i>
+        <hr>
+    <p>&copy; Angel Angelov <?php echo date('Y'); ?></p>
     </footer>
     <script src="js/app.js"></script>
